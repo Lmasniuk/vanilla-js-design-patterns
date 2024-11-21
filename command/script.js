8f28e4978d706f6da1989c4f04c3f55623d54129
Logan Masniuk <logan.masniuk@gmail.com>
2024-11-20 16:12:34 -0800

    add pros for command pattern

--- command/script.js
+++ command/script.js
@@ -7,6 +7,9 @@ Purpose: We can use this pattern to decouple the execution of a method on an obj
 
 Pros
 - The command pattern allows us to decouple methods from the object that executes the operation. It gives you more control if you’re dealing with commands that have a certain lifespan, or commands that should be queued and executed at specific times.
+- undo/redo functionality can be implemented
+- macro commands that bundle multiple commands
+- command queues and scheduling
 
 Cons
 - The use cases for the command pattern are quite limited, and often adds unnecessary boilerplate to an application.
