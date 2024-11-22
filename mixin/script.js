aceb31ed2a39d294c90adf5f4be48d4dbf4b89cf
Logan Masniuk <logan.masniuk@gmail.com>
2024-11-22 11:42:19 -0800

    add start of mixin pattern demo

---
+++ mixin/script.js
@@ -0,0 +1,6 @@
+/*
+Purpose:
+A mixin is an object that we can use in order to add reusable functionality to another object or class, without using inheritance.
+Their sole purpose is to add functionality to objects without inheritance.
+
+*/
